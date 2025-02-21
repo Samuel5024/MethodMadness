3430a668946d1bf8690f71e5dc886b615faaa7ad
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-02-20 22:35:11 -0700

    Update IManager.cs

--- DV1_Arena/Assets/Scripts/IManager.cs
+++ DV1_Arena/Assets/Scripts/IManager.cs
@@ -12,36 +12,5 @@ public interface IManager//declare a public interface
     void Initialize();
 }
 
-public abstract class BaseManager
-{
-    protected string _state;
-    public abstract string state
-    {
-        get;
-        set;
-    }
-    public abstract void Initialize();
-}
-
-public class CombatManager: BaseManager
-{
-    public override string state
-    {
-        get 
-        {
-            return _state;
-        }
-        set
-        {
-            _state = value;
-        }
-
-    }
-    public override void Initialize()
-    {
-        _state = "Manager Initialized...";
-        Debug.Log(_state);
-    }
-}
 
 
